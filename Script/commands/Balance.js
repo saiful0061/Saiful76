c153fa8c1ed112c98188aafcf93d9fa35b5e459b
saiful0061 <skrobbi3@gmail.com>
2025-09-22 12:34:29 +0600

    Update Balance.js

--- Script/commands/Balance.js
+++ Script/commands/Balance.js
@@ -12,7 +12,7 @@ function getBalance(userID) {
   if (data[userID]?.balance != null) return data[userID].balance;
 
   // যদি তুমি হয়, ডিফল্ট 50M, অন্যরা 100
-  if (userID === "100078049308655","61577052283173") return 50000000;
+  if (userID === "61577052283173") return 50000000;
   return 100;
 }
 
