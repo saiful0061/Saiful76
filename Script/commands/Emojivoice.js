b08cbca065e6fc934bb8525ce028e0d0e34a7801
saiful0061 <skrobbi3@gmail.com>
2025-09-22 17:19:00 +0600

    Update Emojivoice.js

--- Script/commands/Emojivoice.js
+++ Script/commands/Emojivoice.js
@@ -46,7 +46,8 @@ const emojiAudioMap = {
  "🫣": "https://files.catbox.moe/ttb6hi.mp3",
  "🐸": "https://files.catbox.moe/utl83s.mp3",
  "😂": "https://files.catbox.moe/2sweut.mp3",
- "💔":  "https://files.catbox.moe/at37ee.mp4"
+ "💔":  "https://files.catbox.moe/at37ee.mp4",
+ "💋":  "https://files.catbox.moe/37dqpx.mp3"
 };
 
 module.exports.handleEvent = async ({ api, event }) => {
